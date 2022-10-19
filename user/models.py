b31dc2736edfb4a953438571171be8076ab6f26d
seungjuyu <2seungju0@gmail.com>
2022-10-19 16:11:20 +0900

    #1 회원가입 기능 구현

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser): 
+    user = models.CharField(max_length=150, null=True)
+    name =models.CharField(max_length=20, null=True)
+    
+    
+   
---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+]
