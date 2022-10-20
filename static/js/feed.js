160fa576d2690280e6cf9bd55d19111a54c7ea97
son950610 <son950610@naver.com>
2022-10-20 12:15:32 +0900

    :lipstick:Design: 메인,작성,상세페이지 스타일수정
    	- 메인페이지 base.html 기존버전으로 재수정
    	- 작성,상세페이지 인스타클론코딩프로젝트 작업사용 및 수정

---
+++ static/js/feed.js
@@ -0,0 +1,43 @@
+// card_feed 모달 
+$('#feedMoreModel').on('show.bs.modal', function(event) {          
+    target_id = $(event.relatedTarget).data('notifyid');
+    // target_auth = $(event.relatedTarget).data('auth');
+
+    // if(target_auth){
+       
+        $(this).find(".btn_edit").show();
+        $(this).find(".btn_deletemodel").show();
+        $(this).find(".btn_unfollow").hide();
+    // }else{
+    //     $(this).find(".btn_edit").hide();
+    //     $(this).find(".btn_deletemodel").hide();
+    //     $(this).find(".btn_unfollow").show();
+    // }
+    $(this).attr("data-target",target_id);
+    $(this).find(".btn_goFeed").attr("onclick","location.href='/diary/"+target_id+"'");
+});
+
+$('#deleteModal').on('show.bs.modal', function(event) {     
+    target_id = $(event.relatedTarget).closest(".modal").attr("data-target")
+    $(this).find(".btn_delete").attr("onclick","location.href='/diary/delete/"+target_id+"'");
+});
+
+$("#staticBackdrop").on("show.bs.modal",function(event){
+    target_id = $(event.relatedTarget).closest(".modal").attr("data-target");
+    if(target_id){
+        // 게시글 수정
+        console.log("edit");
+        $(this).find(".modal-title").text("게시글 수정");
+        let form_url = '/diary/update/'+target_id
+        $(this).find("form").attr("action",form_url);
+        let form_content = $("#feed_"+target_id).find(".feed_content").text();
+        $(this).find("form textarea").val(form_content);
+
+    }else{
+        console.log("create");
+        $(this).find(".modal-title").text("게시글 작성");
+        let form_url = '/diary/'
+        $(this).find("form").attr("action",form_url);
+        $(this).find("form textarea").val("");
+    }
+});
