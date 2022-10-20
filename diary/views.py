61856c6d6146ce38a76fcb79a5229e813331dbeb
LeeHyunji <hyunji015@gmail.com>
2022-10-20 15:49:49 +0900

    ✨Feat: 펫감정예측 머신러닝 결과값 DB에 저장 #7
    - 이미지 업로드시 Diary 모델에 같이 저장
    
    Close #7

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Diary
+from .ml import mainFunc
 
 # Create your views here.
 def main(request):
@@ -16,9 +17,14 @@ def img_upload(request):
         new_diary.image = request.FILES['image']
     except:
         new_diary.image = None
-
     new_diary.save()
-    return redirect('/img-view/'+str(new_diary.id),{'diary':new_diary})
+
+    # 마지막 감정일지의 이미지로 머신러닝로드 
+    last_diary = Diary.objects.latest('id')
+    result = mainFunc(last_diary.image.url)    
+    last_diary.emotion_predict, last_diary.emotion_label, last_diary.emotion_persent = list(result.values())
+    last_diary.save()
+    return redirect('/img-view/'+str(last_diary.id))
     
   else:
     return render(request,'diary/img_upload.html')
