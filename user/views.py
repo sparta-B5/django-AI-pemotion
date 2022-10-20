f5a0e25f40c2040c55c6faeddd4e8856bd765daa
seungjuyu <2seungju0@gmail.com>
2022-10-21 02:05:12 +0900

    :hammer: Rename: 회원가입,로그인 예외 구현 #1
    -1.회원가입
    정규식+ 그외 예외 구현 완료.(비밀번호/아이디 정확하게 )
    
    -2. 로그인
    로그인 구현할때 예외 구현 완료.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from re import fullmatch
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import User
@@ -7,6 +8,7 @@ from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
+
 #회원가입
 def sign_up(request) :
     # 요청방식이 GET(값을읽어올때)이면 html로
@@ -14,33 +16,48 @@ def sign_up(request) :
         return render(request,'user/sign-up.html') 
     # 요청방식이 POST(값을주거나/수정/삭제)이면 회원가입으로
     elif request.method == 'POST':
-        username = request.POST.get('username')
-        name = request.POST.get('name')
-        password = request.POST.get('password')
-        passwordcheck = request.POST.get('passwordcheck')
-        if password == passwordcheck:
-            User.objects.create_user(username=username, password=password, name=name) 
-            return render(request,'user/sign-in.html')
+        username = request.POST.get('username','')
+        name = request.POST.get('name', '')
+        password = request.POST.get('password', '')
+        passwordcheck = request.POST.get('passwordcheck', '')
+        
+        #정규식
+        REGEX_PASSWORD = '^(?=.*[\d])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()])[\w\d!@#$%^&*()]{8,16}$'
+        
+        if password != passwordcheck: 
+            return render(request,'user/sign-up.html',{'error': '패스워드를 확인해 주세요!'})
+        elif username == '':
+            return render(request,'user/sign-up.html',{'error': '아이디는 필수 입니다!'}) 
+        elif password == '':
+            return render(request,'user/sign-up.html',{'error': '비밀번호는 필수 입니다!'})
+        elif username == '' and password == '':
+            return render(request,'user/sign-up.html',{'error': '아이디와 비밀번호는 필수 입니다!'})
+        elif not fullmatch(REGEX_PASSWORD, password): 
+            return render(request,'user/sign-up.html', {'error': '비밀번호는 8~16자리 영문, 숫자, 특수문자 조합만 가능합니다!'} )
         else:
-            return HttpResponse("비밀번호 확인 틀렸습니다")
-            
-       
+            User.objects.create_user(username=username, password=password, name=name)
+            return redirect('/user/sign-in/')
+    
+
 #로그인
 
 def sign_in(request):
     if request.method == "GET":
      return render(request, 'user/sign-in.html')
     elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
         user = authenticate(request, username=username, password=password)
-        
-        if user is not None:
-           login(request, user)   
-           return redirect('/')  
-        else: 
-            return render(request, 'user/sign-in.html')
-    
+        if username == '':
+            return render(request,'user/sign-in.html', {'error': '아이디를 입력해주세요.'} )
+        elif password == '':
+            return render(request,'user/sign-in.html', {'error': '비밀번호를 입력해주세요.'} )
+        elif user is None:
+            return render(request,'user/sign-in.html', {'error': '아이디와 비밀번호 확인 해주세요..'} )
+        elif user is not None:
+            login(request, user)   
+            return redirect('/')
+   
     
 #로그아웃
 def sign_out(request):
