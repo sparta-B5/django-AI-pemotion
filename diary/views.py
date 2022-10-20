ad08f7150edbfb3e9434cfaf59bbf41860c755ec
LeeHyunji <hyunji015@gmail.com>
2022-10-20 19:00:30 +0900

    ✨Feat: 감성일지 전체목록 및 상세 조회  이미지 추가 #11
    이미지가 있는 경우 없는 경우 두타입 작업 완료
    1. 이미지가 있는 게시글
        - 게시글 내용
        - 업로드된 이미지
        - 펫 감정 예측 결과
    2. 이미지가 없는 게시글
        - 게시글 내용
    
    Close #이슈번호

--- diary/views.py
+++ diary/views.py
@@ -70,26 +70,4 @@ def diary_delete(request, id):
     diary.delete()
 
     return redirect('/')
-    
-def img_view(request, id):
-    detail_diary = Diary.objects.get(id=id)
-    return render(request,'diary/img_view.html',{"diary":detail_diary})
 
-def img_upload(request):
-  if request.method=="POST":
-    new_diary = Diary()
-    try:
-        new_diary.image = request.FILES['image']
-    except:
-        new_diary.image = None
-    new_diary.save()
-
-    # 마지막 감정일지의 이미지로 머신러닝로드 
-    last_diary = Diary.objects.latest('id')
-    result = mainFunc(last_diary.image.url)    
-    last_diary.emotion_predict, last_diary.emotion_label, last_diary.emotion_percent = list(result.values())
-    last_diary.save()
-    return redirect('/img-view/'+str(last_diary.id))
-    
-  else:
-    return render(request,'diary/img_upload.html')
