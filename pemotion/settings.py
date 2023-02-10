42cf43ac773d26d2fd3b35570f14440ecf6fbae9
LeeHyunji <hyunji015@gmail.com>
2023-02-10 14:38:08 +0900

    :wrench:Chore: DRF 및 SimpleJWT 패키지추가 #19
    1. 패키지 설치 및 requirements.txt 업데이트
    	- djangorestframowork
    	- djangorestframework-simplejwt

--- pemotion/settings.py
+++ pemotion/settings.py
@@ -38,11 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
     # Custom App
     'user',
     'diary',
     'pet',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
